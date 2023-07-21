8153607cf411caa7a6ab87f5fd43c70b7077fa18
tom <chings228@gmail.com>
2023-07-21 09:05:32 +0800

    Initial Commit

---
+++ BootCamp/BootCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  BootCampApp.swift
+//  BootCamp
+//
+//  Created by man ching chan on 21/7/2023.
+//
+
+import SwiftUI
+
+@main
+struct BootCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
