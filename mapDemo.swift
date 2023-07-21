bff9ccfe4f0ea68b2518a6e6b33a9a09ce1c4a1d
tom <chings228@gmail.com>
2023-07-21 12:06:14 +0800

    map manager

--- mapDemo.swift
+++ mapDemo.swift
@@ -11,16 +11,21 @@ import MapKit
 
 
 
-struct mapDemo: View {
+struct mapDemo: View{
     
-    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 22.2, longitude: 114.11), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
+    @StateObject private var viewModel = ContentViewModel()
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 22.302, longitude: 114.17), span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     
 
     
     var body: some View {
         
         VStack{
-            Map(coordinateRegion:  $region)
+            Map(coordinateRegion:  $region,showsUserLocation: true)
+                .onAppear{
+                    viewModel.checkIfLocationEnabled()
+                }
                 
         }
         .ignoresSafeArea()
@@ -34,4 +39,60 @@ struct mapDemo_Previews: PreviewProvider {
     static var previews: some View {
         mapDemo()
     }
+}
+
+
+
+
+
+
+
+
+final class ContentViewModel:NSObject,ObservableObject,CLLocationManagerDelegate{
+    
+    var locationManager: CLLocationManager!
+    
+
+    
+   func checkIfLocationEnabled(){
+        
+        if CLLocationManager.locationServicesEnabled(){
+            
+            locationManager = CLLocationManager()
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.delegate = self
+            
+            checkLocationAuthorization()
+        }
+        else{
+            print("no location")
+        }
+    }
+    
+    private func checkLocationAuthorization(){
+        
+        guard let locationManager = locationManager else {return}
+        
+        switch locationManager.authorizationStatus{
+            
+        case .notDetermined:
+            print(".notdetermined")
+            print("do something")
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print(".restried")
+        case .denied:
+            print(".denied")
+        case .authorizedAlways:
+            print(".authorizationalways")
+        case .authorizedWhenInUse:
+            print(".authorizationinuse")
+        @unknown default:
+            print(".unknown")
+        }
+    }
+    
+    
+
+    
 }
